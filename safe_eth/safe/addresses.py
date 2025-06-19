1eed365924ab3066ba978095d862f4709b981b20
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2025-06-19 22:38:17 +0300

    Add new addresses for version 1.4.1 and 1.4.1+L2 to BOBA_NETWORK

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -371,6 +371,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.BOBA_NETWORK: [
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 170908, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 170910, "1.3.0"),
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 11367668, "1.4.1"),  # v1.4.1
+        (
+            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+            11367672,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
     ],
     EthereumNetwork.AURORA_MAINNET: [
         (
@@ -2351,6 +2357,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 55739),  # v1.3.0
     ],
     EthereumNetwork.BOBA_NETWORK: [
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 11367636),  # v1.4.1
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 170895),  # v1.3.0
     ],
     EthereumNetwork.AURORA_MAINNET: [
